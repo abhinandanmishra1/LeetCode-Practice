e6ce6815dce8509b20765e5afbf9ef8090b23f18
abhinandanmishra1 <64205626+abhinandanmishra1@users.noreply.github.com>
2022-02-20 19:42:32 +0530

    Time: 3 ms (33.81%), Space: 6.6 MB (59.32%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> store={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    void findans(string digits,int i,vector<string>& ans,string s){
+        if(i==digits.size()){
+            ans.push_back(s);
+            return;
+        }
+        int x=digits[i]-'0';
+        for(char c:store[x]){
+            findans(digits,i+1,ans,s+c);
+        }
+    }
+    
+    vector<string>  letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.size()==0) return ans;
+        findans(digits,0,ans,"");
+        return ans;
+    }
+};
+
+
+
+/*
+Idea - 
+Define a new mobile keyboard in which all the alphabets of english can be 
+*/
