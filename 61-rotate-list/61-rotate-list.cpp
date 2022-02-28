33ab4ab3360b0df79d1d4a68e12e7f37fac5784a
Abhinandan <64205626+abhinandanmishra1@users.noreply.github.com>
2022-02-28 10:09:31 +0530

    Time: 13 ms (38.75%), Space: 11.8 MB (56.15%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.cpp
@@ -0,0 +1,42 @@
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head or !head->next or k==0) return head;
+        ListNode *t1=head;
+        int n=0;
+        ListNode *t2=head->next;
+        while(t1){
+            n++;
+            t1=t1->next;
+        }
+        
+        k=k%n;
+        if(k==0) return head;
+        int i=0;
+        t1=head;
+        k=n-k;
+        cout<<k<<endl;
+        while(i<k-1){
+            t1=t1->next;
+            t2=t2->next;
+            i++;
+        }
+        ListNode *ans=t2;
+        t1->next=NULL;
+        while(t2!=NULL and t2->next!=NULL and t2->next->next!=NULL){
+            t2=t2->next;
+        }
+        cout<<(t2->val)<<endl;
+        if(!t2->next)
+        {t2->next=head;
+        cout<<"yes";
+        }
+        else if(!t2->next->next){
+            t2->next->next=head;
+            cout<<"yesss";
+        }
+        return ans;
+        
+    }
+};
