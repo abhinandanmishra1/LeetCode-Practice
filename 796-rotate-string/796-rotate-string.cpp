dfa9c270463a82d8e7844073824966b92d9f9d83
Abhinandan <64205626+abhinandanmishra1@users.noreply.github.com>
2022-02-28 22:42:33 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (29.79%) - LeetHub

--- 796-rotate-string/796-rotate-string.cpp
+++ 796-rotate-string/796-rotate-string.cpp
@@ -1,33 +1,11 @@
 class Solution {
 public:
     bool rotateString(string s1, string s2) {
-        /*
-            We have to check whether the string s1 can be converted 
-            to string s2 after some shifts.
-            Shifts means shifting leftmost char from s1 to rightmost position
-            
-            s1- abcde
-            s2- cdeab
-            
-            Shift one char of s1
-            s1 - bcdea
-            
-            Shift one char from s1
-            s1- cdeab  -- this is equal to s2
-            
-            Intuition - 
-            We can check for each rotation also.
-            
-            But if we double s2 by s2=s2+s2 
-            that is for above example - s2= bcdeabcdea
-            then if s1 is a substring of the new s2 then return true else return false;
-            
-            We can use KMP algorithm for that. 
-        */
         s2+=s2;
         int m=s1.length();
-        int n=s2.length();  // s2 is greater
-        if(n<=m) return false; 
+        int n=s2.length();  
+        if(n<m) return false; 
+        if(n==2 and m==2) return false;
         
         vector<int> store(m,0);
         int i=0,j=1;
@@ -45,7 +23,6 @@ public:
             }
         }
         
-        // Now traverse both the strings using store array 
         i=0,j=0;
         while(i<n){
             while(j<m){
@@ -57,7 +34,6 @@ public:
                 }
             }
             if(j==m){
-                // substring is found 
                 return true;
             }
             if(j>0){
@@ -66,8 +42,6 @@ public:
                 i++;
             }
         }
-        return false;
-    
-        
+        return false; 
     }
 };
