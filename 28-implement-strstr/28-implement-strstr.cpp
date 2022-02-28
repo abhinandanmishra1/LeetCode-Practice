b1f4c23f0f09d2d93ed7256c12f7a67976d1971a
Abhinandan <64205626+abhinandanmishra1@users.noreply.github.com>
2022-02-28 20:30:01 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (31.18%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,49 +1,62 @@
 class Solution {
 public:
     int strStr(string s1, string s2) {
+        // I have to return the first occurrence of s2 in s1
        int n=s1.length();
-       int m=s2.length();
-       if(s1==s2) return 0;
-       if(n<m) return -1;
-       vector<int> store(m,0);
-       int i=0,j=1;
-       while(j<m){
-           
-           if(s2[i]==s2[j]){
-               store[j]=i+1;
-               i++;
-               j++;
-           }else{
-               if (i>0)
-                    i = store[i - 1];
-               else{ 
-                    store[j] = 0;
-                    j++;
-                 }
-           }
-           
-       }
+       int m =s2.length();
+       // for this I can use KMP algorithm 
+       // Let's understand how it works
+        if(n<m) return -1;
+        if(s1==s2) return 0;
+    /*
+       s1=hello
+       s2=ll 
        
-      i=0,j=0;
-      while(i<n){
-          while(j<m){
-              if(s1[i]==s2[j]){
-                  i++,j++;
-              }else{
-                  break;
-              }
-          }
-          cout<<j<<" ";
-          if(j==m){
-              return i-m;
-          }
-          if(j>0){
-              j=store[j-1];
-          }else{
-              i++;
-          }
-          
-      }
-        return -1;
+       we have to make a store array which will store
+       the s2 indices where prefix of the string is equal to suffix
+       for each substring of (0 to i) (where i=0 to m)
+    */
+      vector<int> store(m,0);
+      int i=0,j=1;
+        while(j<m){
+            if(s2[i]==s2[j]){
+                i++;
+                store[j]=i;  // length = i+1
+                j++;
+            }else{
+                if(i>0){
+                    i=store[i-1];
+                }else{
+                    j++;
+                }
+            }
+            
+        }
+        // now our array is maintained 
+        // Main Kmp traversal 
+        
+        i=0,j=0;
+        while(i<n){
+            while(j<m){
+                if(s1[i]==s2[j]){
+                    i++;
+                    j++;
+                }else{
+                    break;
+                }
+            }
+            if(j==m){
+                // patter is found
+                return i-m; // return starting index that is i-m
+            }
+            
+            if(j>0){
+                j=store[j-1];
+            }else{
+                i++;
+            }
+        }
+        return -1; /// if pattern not found
+        
     }
 };
