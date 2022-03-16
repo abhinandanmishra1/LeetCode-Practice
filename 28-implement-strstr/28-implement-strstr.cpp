0f0cf34ee17a1b890b81483d4e82fd4687fde3a0
Abhinandan <64205626+abhinandanmishra1@users.noreply.github.com>
2022-03-16 11:21:09 +0530

    Time: 17 ms (64.97%), Space: 8.3 MB (19.24%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int strStr(string s1, string s2) {
-        // I have to return the first occurrence of s2 in s1
-       if(s1==s2 or s2.length()==0) return 0;
+    
+        
+       if((s1.size()==1 and s2.length()<=1) or (s1.length()==0 and s2.length()==0)) return 0;
        string tot=s2+"$"+s1;
        int n=tot.length();
         vector<int> store(n,0);
         int l=0,r=0;
-        // cout<<tot<<endl;
+       
         for(int i=0;i<n;i++){
             if(i>r){
-                // cout<<"isme\n";
                 l=i,r=i;
                 while(r<n and tot[r]==tot[r-l]) r++;
                 store[i]=r-l;
                 r--;
             }else{
-                // cout<<"usme\n";
                 int ind=i-l;
                 if(i+store[ind]<=r) store[i]=store[ind];
                 else{
