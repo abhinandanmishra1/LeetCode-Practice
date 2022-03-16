f032338a82f166d69190621ab7a3545e944d7683
Abhinandan <64205626+abhinandanmishra1@users.noreply.github.com>
2022-03-16 11:13:15 +0530

    Time: 8 ms (84.98%), Space: 8.3 MB (19.80%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -2,34 +2,34 @@ class Solution {
 public:
     int strStr(string s1, string s2) {
         // I have to return the first occurrence of s2 in s1
-        if(s1==s2) return 0;
-       int n=s1.length();
-       int m =s2.length();
-       if(n<m) return -1;
-        vector<int> store(m,0);
-        int i=0,j=1;
-        while(j<m){
-            if(s2[i]==s2[j]){
-                store[j++]=++i;
+       if(s1==s2 or s2.length()==0) return 0;
+       string tot=s2+"$"+s1;
+       int n=tot.length();
+        vector<int> store(n,0);
+        int l=0,r=0;
+        // cout<<tot<<endl;
+        for(int i=0;i<n;i++){
+            if(i>r){
+                // cout<<"isme\n";
+                l=i,r=i;
+                while(r<n and tot[r]==tot[r-l]) r++;
+                store[i]=r-l;
+                r--;
             }else{
-                if(!i){
-                    j++;
-                }else{
-                    i=store[i-1];
+                // cout<<"usme\n";
+                int ind=i-l;
+                if(i+store[ind]<=r) store[i]=store[ind];
+                else{
+                    l=i;
+                    while(r<n and tot[r]==tot[r-l]) r++;
+                    store[i]=r-l;
+                    r--;
                 }
             }
         }
-        i=0,j=0;
-        while(i<n){
-            while(j<m and s1[i]==s2[j]){
-                i++;
-                j++;
-            }
-            if(j==m) return i-m;
-            if(j>0){
-                j=store[j-1];
-            }else{
-                i++;
+        for(int i=0;i<n;i++){
+            if(store[i]==s2.length()){
+                return i-s2.length()-1;
             }
         }
         return -1;
