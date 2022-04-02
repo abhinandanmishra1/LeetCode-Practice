ed7cfb8155dfcdec40b71662e064fdd3b0077778
Abhinandan <64205626+abhinandanmishra1@users.noreply.github.com>
2022-04-02 10:05:29 +0530

    Time: 55 ms (91.88%), Space: 30.3 MB (6.42%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool palindrome(string &s){
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(s[i++]!=s[j--]) return false;
+            
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        if(palindrome(s)) return true;
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(s[i]!=s[j]) break;
+            i++;
+            j--;
+        }
+        string left=s.substr(0,i);
+        string right=s.substr(i+1);
+        string ans=left+right;
+        if(palindrome(ans)) return true;
+        left=s.substr(0,j);
+       right=s.substr(j+1);
+       ans=left+right;
+        if(palindrome(ans)) return true;
+        return false;
+    }
+};
