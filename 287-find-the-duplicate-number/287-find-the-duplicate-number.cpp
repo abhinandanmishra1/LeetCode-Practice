2fc3e094d33ff2aec4cac59e457e52ebaf4aae51
Abhinandan <64205626+abhinandanmishra1@users.noreply.github.com>
2022-05-07 09:04:17 +0530

    Time: 158 ms (52.48%), Space: 61.3 MB (61.98%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(auto x:nums){ mp[x]++;
-                         if(mp[x]==2) return x;
-                        }
-        return -1;
+        int slow=nums[0],fast=nums[nums[0]];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[nums[fast]]; // faster than slow by 1
+        }
+        
+        // Now we get the first intersection 
+        // Now we start moving from 0th position in forward direction
+        // and also from the first intersection point in forward direction 
+        // and the second intersection that we get will be the answer
+        // This floyd's algorithm
+        
+        int second_iter=0;
+        while(second_iter!=fast){
+            second_iter=nums[second_iter];
+            fast=nums[fast];
+        }
+        return fast;
     }
 };
