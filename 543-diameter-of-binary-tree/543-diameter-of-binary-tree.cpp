9dc0d8ddf1ec85458ab1739f7de9943b65a63dab
Abhinandan Mishra <64205626+abhinandanmishra1@users.noreply.github.com>
2022-05-22 20:36:01 +0530

    Time: 11 ms (76.85%), Space: 20.2 MB (93.00%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int ans=INT_MIN;
-    int findans(TreeNode * root){
-        if(!root){
-            return 0;
-        }
-        int lefth=findans(root->left);
-        int righth=findans(root->right);
-        ans=max(ans,lefth+righth);
-        return max(lefth,righth)+1;
+    int ans=0;
+    int findDiameter(TreeNode *root){
+        if(!root) return 0;
+        // findDiameter returns the height of the tree
+        // if we know height of both left and right tree 
+        // then we can store the ans = max(ans,left+right+1)
+        int leftHeight=findDiameter(root->left);
+        int rightHeight=findDiameter(root->right);
+        
+        ans=max(ans,leftHeight+rightHeight);
+        
+        return max(leftHeight,rightHeight)+1; // as height of a tree is maximum height of left or right subtree + 1
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        int x=findans(root);
+        findDiameter(root);
         return ans;
     }
 };
