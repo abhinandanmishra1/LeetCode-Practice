46c471ec3b2f88eb554ba57dbd0a325561514205
Abhinandan Mishra <64205626+abhinandanmishra1@users.noreply.github.com>
2022-05-22 19:15:18 +0530

    Time: 94 ms (48.22%), Space: 51.6 MB (5.36%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,4 +1,5 @@
 // Approach 1
+/*
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
@@ -11,6 +12,28 @@ public:
                 for(int j=i+1;j<=end;j++) visited[j]=true;
             }
         }
+        return false;
+    }
+};
+*/
+
+// Approach 2
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        // If i add the indexes i to each nums[i] 
+        priority_queue<int> prevLargestRange;
+        prevLargestRange.push(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(prevLargestRange.top()>=nums.size()-1) return true;
+            if(prevLargestRange.top()>=i){
+                 int currentRange=nums[i]+i;
+                 prevLargestRange.push(currentRange);
+            }else{
+                 return false;
+            }
+        }
+        if(prevLargestRange.top()>=nums.size()-1) return true;
         return false;
     }
 };
