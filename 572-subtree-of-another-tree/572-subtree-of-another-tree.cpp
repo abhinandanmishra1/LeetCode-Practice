269bd57ae43b893de4110d5550b8d1ba4fe1f287
Abhinandan Mishra <64205626+abhinandanmishra1@users.noreply.github.com>
2022-05-24 20:54:49 +0530

    Time: 46 ms (20.34%), Space: 28.7 MB (96.41%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -2,14 +2,27 @@
 class Solution {
 public:
     
-    bool isSubtree(TreeNode* root, TreeNode* subRoot,bool direct=false) {
-       if(root==NULL and subRoot==NULL) return true;
-       if(root==NULL or subRoot==NULL) return false;
-        bool ans=false;
-       if(root->val==subRoot->val){
-           ans=(isSubtree(root->left,subRoot->left,true) and isSubtree(root->right,subRoot->right,true));
-       }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot,bool checkDirect=false) {
+        if(subRoot==NULL and root==NULL) return true;
+        if(subRoot==NULL or root==NULL) return false;
         
-        return ans or (!direct and isSubtree(root->left,subRoot) or isSubtree(root->right,subRoot));
+        if(checkDirect){
+            
+            if(root->val==subRoot->val){
+                // if 
+                return (isSubtree(root->left,subRoot->left,true) and isSubtree(root->right,subRoot->right,true));
+            }else{
+                //If some prev elements are matched but current element is not matched
+                return false;
+            }
+            
+        }else{
+           if(root->val==subRoot->val){
+               return (isSubtree(root->left,subRoot->left,true) and isSubtree(root->right,subRoot->right,true)) or isSubtree(root->left,subRoot,false) or isSubtree(root->right,subRoot,false);
+           }else{
+               return isSubtree(root->left,subRoot,false) or isSubtree(root->right,subRoot,false);
+           }
+        }
+        
     }
 };
