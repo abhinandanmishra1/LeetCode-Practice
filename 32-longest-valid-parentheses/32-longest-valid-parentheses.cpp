0cc3b02d40775bd5add80a13dcb3f538d43b1aa8
Abhinandan Mishra <64205626+abhinandanmishra1@users.noreply.github.com>
2022-05-24 19:08:25 +0530

    Time: 7 ms (47.05%), Space: 6.9 MB (88.01%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -3,29 +3,38 @@ public:
     int longestValidParentheses(string s) {
         int n=s.length();
         int ans=0;
-        stack<int> st;
-        
-        st.push(-1);
+        int open=0,close=0;
         for(int i=0;i<n;i++){
-                    if(s[i]=='('){
-                        st.push(i);
-                    }else{
-                        
-                        st.pop();
-                        if(st.empty()){
-                            st.push(i);
-                        }
-                        else{
-                            ans=max(ans,i-st.top());
-                        }
-                        
-                        
-                    }
+            if(s[i]=='('){
+                open++;
+            }else{
+                close++;
+            }
+            // if open is less then keep adding 
+            // if open is greater than close then there is chance that 
+            // close will increase and become equal to open
+            // if close is greater at some point that means this 
+            // substring is not valid now intialize with open=close=0
+            // if both are equal --> store max(ans,open+close) 
+            if(open==close) ans=max(ans,open+close);
+            else if(close>open) open=close=0;
         }
-        
-    
+        open=close=0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='('){
+                open++;
+            }else{
+                close++;
+            }
+            // if close is less then keep adding 
+            // if close is greater than open then there is chance that 
+            // open will increase and become equal to close
+            // if open is greater at some point that means this 
+            // substring is not valid now intialize with open=close=0
+            // if both are equal --> store ans=max(ans,open+close) 
+            if(open==close) ans=max(ans,open+close);
+            else if(close<open) open=close=0;
+        }
         return ans;
-        
-        
     }
 };
