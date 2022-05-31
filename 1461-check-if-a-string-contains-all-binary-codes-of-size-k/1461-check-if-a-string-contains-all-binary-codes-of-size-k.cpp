d4c3889c8bab760ecfe8fbefd409cd252f39bd7a
Abhinandan Mishra <64205626+abhinandanmishra1@users.noreply.github.com>
2022-05-31 18:39:19 +0530

    Time: 512 ms (44.97%), Space: 29 MB (86.16%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -4,28 +4,35 @@ public:
         set<int> st;
         int f=0;
         int n=s.length();
-        if((1<<k)>(n+1-k)) return 0;
+        // checking if number of substrings of length k is less than required number
+        // then answer will be false or 0 
+        int requiredCounts=1<<k;
+        if((requiredCounts)>(n+1-k)) return 0;
         int end=n-k;
         int start=n-1;
-        int j=0;
+        int j=1;
+        // K times
         while(start>=end){
             if(s[start]=='1'){
-                f+=(1<<j);
+                f+=j;
             }
-            j++;
+            j*=2;
             start--;
         }
-       
+        // Now in f the value of last k bits in integer format will be stored
         st.insert(f);
         start=n-k-1;
+        // each time I'm removing last bit 
+        // adding kth bit into it 
+        int kthBit=(1<<(k-1));
         while(start>=0){
-            f=f>>1;
-            if(s[start]=='1'){
-                f+=(1<<(k-1));
+            f=f>>1; // removing the last bit 
+            if(s[start]=='1'){ // if kth bit is set then add it to the answer
+                f+=kthBit;
             }
             st.insert(f);
             start--;
         }
-        return st.size()==(1<<k);
+        return st.size()==requiredCounts;
     }
 };
