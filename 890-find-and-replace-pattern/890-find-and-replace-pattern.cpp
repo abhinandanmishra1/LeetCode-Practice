66f2b149e45051c5c40ea6a4450e9e7975de1fad
Abhinandan Mishra <64205626+abhinandanmishra1@users.noreply.github.com>
2022-07-29 09:49:16 +0530

    Time: 4 ms (76.53%), Space: 8.6 MB (43.97%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
-        vector<string> ans;
+        
+        vector<string> newArray;
+        
         for(auto &word:words){
-            unordered_map<char,char> mp;
-            unordered_map<char,char> visited;
-            int i=0;
-            for(i=0;i<pattern.length();i++){
-                if(mp.find(pattern[i])!=mp.end()){
-                    if(mp[pattern[i]]!=word[i]) break;
-                }else{
-                    if(visited.find(word[i])!=visited.end()){
-                        if(visited[word[i]]!=pattern[i]) break;
+            unordered_map<char,char> patternToWord;
+            bool add=true;
+            for(int i=0;i<pattern.length();i++){
+                if(patternToWord.find(pattern[i])!=patternToWord.end()){
+                    if(patternToWord[pattern[i]]!=word[i]){
+                        add=false;
+                        break;
                     }
-                    mp[pattern[i]]=word[i];
-                    visited[word[i]]=pattern[i];
+                }else{
+                    patternToWord[pattern[i]]=word[i];
                 }
             }
             
-            if(i==pattern.length()){
+            if(add){
+                newArray.push_back(word);
+            }
+        }
+        
+        vector<string> ans;
+        
+        for(auto &word:newArray){
+            unordered_map<char,char> wordToPattern;
+            bool add=true;
+            for(int i=0;i<pattern.length();i++){
+                if(wordToPattern.find(word[i])!=wordToPattern.end()){
+                    if(wordToPattern[word[i]]!=pattern[i]){
+                        add=false;
+                        break;
+                    }
+                }else{
+                    wordToPattern[word[i]]=pattern[i];
+                }
+            }
+            
+            if(add){
                 ans.push_back(word);
             }
         }
+        
         return ans;
     }
 };
