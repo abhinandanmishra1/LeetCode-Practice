325a8841bf135298ab2ae9b13f430aa91c2108d2
Abhinandan Mishra <64205626+abhinandanmishra1@users.noreply.github.com>
2022-07-30 09:18:55 +0530

    Time: 903 ms (14.61%), Space: 161.8 MB (19.55%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        unordered_map<char,int> store;
+        for(auto &word:words2){
+            
+            unordered_map<char,int> counter;
+            for(auto x:word){
+                counter[x]++;
+                store[x]=max(store[x],counter[x]);
+            }
+            
+        }
+        
+        vector<string> ans;
+        
+        for(auto &word:words1){
+            unordered_map<char,int> counter;
+            for(auto x:word){
+                counter[x]++;
+            }
+            bool add=true;
+            for(auto letter_count:store){
+                if(counter[letter_count.first]<letter_count.second){
+                    add=false;
+                    break;
+                }
+            }
+            if(add){
+                ans.push_back(word);
+            }
+        }
+        return ans;
+    }
+};
