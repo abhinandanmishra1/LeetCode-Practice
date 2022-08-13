6073d425d3e7fe4e4abd8e29add89e94811e165d
Abhinandan Mishra <64205626+abhinandanmishra1@users.noreply.github.com>
2022-08-13 11:46:34 +0530

    Time: 12 ms (53.76%), Space: 8.3 MB (71.66%) - LeetHub

--- 482-license-key-formatting/482-license-key-formatting.cpp
+++ 482-license-key-formatting/482-license-key-formatting.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
     string licenseKeyFormatting(string s, int k) {
-        string unformat="";
-        int i=0;
-        while(i<s.length()){
+        string ans="";
+        
+        int i=s.length()-1;
+        int cnt=0;
+        while(i>=0){
             if(s[i]!='-'){
-                if(s[i]>='a' and s[i]<='z'){
-                    unformat.push_back(s[i]-'a'+'A');
-                }else{
-                    unformat.push_back(s[i]);
+                if(cnt==k){
+                    ans.push_back('-');
+                    cnt=0;
                 }
+                if(s[i]>='a' and s[i]<='z'){
+                    ans.push_back(s[i]-'a'+'A');
+                }else{
+                    ans.push_back(s[i]);
+                }
+                cnt++;
             }
-            i++;
+            i--;
         }
         
-        int len=unformat.size();
-        if(len==0) return "";
-        string ans="";
-        int first=len%k;
-        first=first==0?k:first;
-        ans.push_back(unformat[0]);
-        i=1;
-        while(i<len){
-            if(i==first){
-                ans.push_back('-');
-                first+=k;
-            }
-            ans.push_back(unformat[i++]);
-        }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
