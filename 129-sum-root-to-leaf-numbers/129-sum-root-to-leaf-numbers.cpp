57285390ad9fa247ca2616fe090165332a9f7398
Abhinandan Mishra <64205626+abhinandanmishra1@users.noreply.github.com>
2022-09-19 23:21:06 +0530

    Time: 6 ms (31.81%), Space: 9.2 MB (83.56%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.cpp
@@ -1,39 +1,10 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<int> ans;
-    void findsum(TreeNode *root,int curr){
-        if(!root) return;
-        if(!root->left and !root->right){
-            // cout<<curr<<endl;
-            ans.push_back(curr*10+root->val);
-            return;
-        }
-        int currSum=curr*10+root->val;
-        if(root->left)
-        findsum(root->left,currSum);
-        if(root->right)
-        findsum(root->right,currSum);
-    }
-    int sumNumbers(TreeNode* root) {
+    int sumNumbers(TreeNode* root,int sum=0) {
         if(!root) return 0;
-        if(!root->left and !root->right){
-            ans.push_back(root->val);
-        }
-        findsum(root->left,root->val);
-        findsum(root->right,root->val);
-        int total=0;
-        for(auto x:ans) total+=x;
-        return total;
+        if(!root->left and !root->right) return sum*10+root->val;
+        int leftNumber = sumNumbers(root->left,sum*10+root->val);
+        int rightNumber = sumNumbers(root->right,sum*10+root->val);        
+        return leftNumber + rightNumber;
     }
 };
