7e665e32c5a47c84635ea376280fc9235745eafa
Abhinandan Mishra <64205626+abhinandanmishra1@users.noreply.github.com>
2022-09-24 01:05:30 +0530

    Time: 379 ms (93.56%), Space: 180.2 MB (63.53%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -11,20 +11,32 @@
  */
 class Solution {
 public:
-    int ans=0;
-    void findans(TreeNode *root,long int path){
-        if(!root) return;
-        if(!root->left and !root->right){
-            path=path^(1<<root->val);
-            if(__builtin_popcount(path)<=1) ans++;
-            return;
+    bool isPseudoPalindromic(vector<int> &count){
+        int oddCount=0;
+        for(int i=1;i<10;i++){
+            oddCount+=count[i]&1;
+            if(oddCount>1) return false;
         }
-        findans(root->left,path^(1<<root->val));
-        findans(root->right,path^(1<<root->val));
+        return true;
+    }
+    int countPseudoPalindromicPaths(TreeNode *root,vector<int> &count){
+        if(!root){
+            return 0;
+        }
+        if(!root->left and !root->right){
+            count[root->val]++;
+            int ans = isPseudoPalindromic(count);
+            count[root->val]--;
+            return ans;
+        }
+        count[root->val]++;
+        int leftAns=countPseudoPalindromicPaths(root->left,count);
+        int rightAns=countPseudoPalindromicPaths(root->right,count);
+        count[root->val]--;
+        return leftAns+rightAns;
     }
     int pseudoPalindromicPaths (TreeNode* root) {
-        long int path=0;
-        findans(root,path);
-        return ans;
+        vector<int> count(10,0);
+        return countPseudoPalindromicPaths(root,count);
     }
 };
