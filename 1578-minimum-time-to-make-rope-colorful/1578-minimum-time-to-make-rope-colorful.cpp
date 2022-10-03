19c2a2ac64d12225caa56deff001a7c6917fe1f4
Abhinandan Mishra <64205626+abhinandanmishra1@users.noreply.github.com>
2022-10-03 22:57:28 +0530

    Time: 353 ms (27.16%), Space: 95.4 MB (50.05%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minCost(string clr, vector<int>& nt) {
+        int ans=0;
+        
+        int i=0,n=clr.length();
+        int curr=nt[i++];
+        int mx=nt[0];
+        while(i<n){
+            if(clr[i]==clr[i-1]){
+                curr+=nt[i];
+                mx=max(mx,nt[i++]);
+            }else{
+                ans+=(curr-mx);
+                curr=nt[i];
+                mx=nt[i];
+                i++;
+            }
+        }
+        ans+=(curr-mx);
+        return ans;
+    }
+};
