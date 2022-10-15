f11cff6f9b750c7b00fb07687e55516cda601b5d
Abhinandan Mishra <64205626+abhinandanmishra1@users.noreply.github.com>
2022-10-16 00:10:31 +0530

    Time: 0 ms (100.00%), Space: 6 MB (61.40%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int dp[2][60];
+    int dp[60];
     int getMaxAns(int n,int x=0){
         if(n<=0) return 0;
         if(n==1) return 1;
-        if(dp[x][n]!=-1) return dp[x][n];
+        if(dp[n]!=-1) return dp[n];
         int ans=0;
         for(int i=1;i<=n;i++){
             ans=max(ans,i*getMaxAns(n-i,1));
@@ -12,7 +12,7 @@ public:
                 ans=max({ans,i*(n-i),n});
             }
         }
-        return dp[x][n]=ans;
+        return dp[n]=ans;
     }
     int integerBreak(int n) {
       memset(dp,-1,sizeof(dp));
